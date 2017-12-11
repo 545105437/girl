7ed5f176e49d6310c468021f6a9fa384ecde4676
wal <545105437@qq.com>
2017-12-11 14:06:21 +0800

    创建版本库

---
+++ src/main/java/com/demo/GirlApplication.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 入口
+ *
+ * @author wal
+ * @date 2017/12/11
+ */
+@SpringBootApplication
+public class GirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlApplication.class, args);
+	}
+}
